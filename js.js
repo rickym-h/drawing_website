b2dc7e07245c74c98bf5617054027e3e64ed9ab2
rickym-h <richardhull11@gmail.com>
2022-06-15 14:08:06 +0100

    made grid update when clicked

--- js.js
+++ js.js
@@ -1,9 +1,13 @@
+let IS_MOUSE_PRESSED = false;
+
 function generateGrid() {
     const container = document.querySelector("#container");
     let size = NaN;
     do {
         size = Number(prompt("Please enter an integer between 1-100: ", "20"));
+
         console.log(size);
+        // todo make an isValidSize(size) to validate.
     } while (isNaN(size) && size >= 1 && size <= 100)
 
     // delete old nodes
@@ -19,8 +23,15 @@ function generateGrid() {
         for (let col = 0; col < size; col++) {
             const myNode = document.createElement("div");
             myNode.classList.toggle("defaultNode");
+
+            myNode.addEventListener("mousedown", function(e) {
+                console.log(e);
+                e.target.classList.toggle("defaultNode")
+                e.target.classList.toggle("colouredNode")
+            })
+
+
             container.appendChild(myNode);
-            console.log("making node: row="+row+" col="+col)
         }
     }
 

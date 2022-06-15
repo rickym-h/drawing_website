cd3664020020e338781d3979f496cbe980693fe5
rickym-h <richardhull11@gmail.com>
2022-06-15 13:58:08 +0100

    made css grid dynamically update depending on input

--- js.js
+++ js.js
@@ -6,13 +6,24 @@ function generateGrid() {
         console.log(size);
     } while (isNaN(size) && size >= 1 && size <= 100)
 
-    for (let row = 0; row <= size; row++) {
-        for (let col = 0; col <= size; col++) {
+    // delete old nodes
+    container.innerHTML = '';
+
+    //set styling taking into account the new nodes
+    container.style.gridTemplateRows = "repeat("+size.toString()+", 1fr)";
+    container.style.gridTemplateColumns = "repeat("+size.toString()+", 1fr)";
+
+
+    // Create grid
+    for (let row = 0; row < size; row++) {
+        for (let col = 0; col < size; col++) {
             const myNode = document.createElement("div");
             myNode.classList.toggle("defaultNode");
             container.appendChild(myNode);
+            console.log("making node: row="+row+" col="+col)
         }
     }
+
 }
 
 const myButton = document.querySelector("#myButton");

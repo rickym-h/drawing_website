2098a681c64133cc1a66be2a1415c90fb65bab96
rickym-h <richardhull11@gmail.com>
2022-06-15 14:30:47 +0100

    added joke button to draw a custom image on the grid

--- js.js
+++ js.js
@@ -6,7 +6,6 @@ function generateGrid() {
     do {
         size = Number(prompt("Please enter an integer between 1-100: ", "20"));
 
-        console.log(size);
         // todo make an isValidSize(size) to validate.
     } while (isNaN(size) && size >= 1 && size <= 100)
 
@@ -57,4 +56,67 @@ document.addEventListener("mousedown", function (e) {
 })
 document.addEventListener("mouseup", function (e) {
     IS_MOUSE_PRESSED = false;
+})
+
+const myFunnyButton = document.querySelector("#jokeButton");
+
+function isPartOfPic(row, col) {
+    let myPic =
+        [[0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,1,0,0,0,0],
+        [0,0,0,0,0,1,0,0,0,0],
+        [0,0,0,0,0,1,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0]]
+
+    return myPic[row][col] === 1;
+
+}
+
+myFunnyButton.addEventListener("click", function(e){
+    let size = 10
+    // delete old nodes
+    container.innerHTML = '';
+
+    //set styling taking into account the new nodes
+    container.style.gridTemplateRows = "repeat("+size.toString()+", 1fr)";
+    container.style.gridTemplateColumns = "repeat("+size.toString()+", 1fr)";
+
+
+    // Create grid
+    for (let row = 0; row < size; row++) {
+        for (let col = 0; col < size; col++) {
+            const myNode = document.createElement("div");
+            if (isPartOfPic(row,col)) {
+                myNode.classList.toggle("colouredNode")
+            } else {
+                myNode.classList.toggle("defaultNode");
+            }
+
+
+            myNode.addEventListener("mouseenter", function (e) {
+                if (IS_MOUSE_PRESSED) {
+                    e.target.classList.remove("defaultNode");
+                    e.target.classList.add("colouredNode");
+                }
+            })
+
+            myNode.addEventListener("mousedown", function (e) {
+                e.target.classList.remove("defaultNode");
+                e.target.classList.add("colouredNode");
+                IS_MOUSE_PRESSED = true;
+            })
+
+            myNode.addEventListener("mouseup", function (e) {
+                IS_MOUSE_PRESSED = false;
+            })
+
+
+            container.appendChild(myNode);
+        }
+    }
 })
